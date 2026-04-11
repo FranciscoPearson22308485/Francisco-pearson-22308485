f890dea31413578c0eb03454c1b420324db91d3f
FranciscoPearson22308485 <a22308485@alunos.ulht.pt>
2026-04-11 00:07:57 +0000

    Configurado urls.py para servir ficheiros media

--- project/urls.py
+++ project/urls.py
@@ -17,7 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+# Rota para o Django servir os ficheiros da pasta media durante o desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
