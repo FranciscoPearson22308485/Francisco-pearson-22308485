05d04e1eff692e58a4448044fdcfbf52506cfa82
FranciscoPearson22308485 <a22308485@alunos.ulht.pt>
2026-04-24 17:27:01 +0000

    Implementa listagem de Unidades Curriculares com otimização de queries

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,4 +6,5 @@ app_name = 'portfolio'
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('tecnologias/', views.tecnologias_view, name='tecnologias'),
+    path('ucs/', views.ucs_view, name='ucs'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Tecnologia
+from .models import Tecnologia, UnidadeCurricular
 
 def home_view(request):
     return render(request, 'portfolio/home.html')
@@ -7,3 +7,7 @@ def home_view(request):
 def tecnologias_view(request):
     tecnologias = Tecnologia.objects.all()
     return render(request, 'portfolio/tecnologias.html', {'tecnologias': tecnologias})
+
+def ucs_view(request):
+    ucs = UnidadeCurricular.objects.select_related('licenciatura').prefetch_related('docentes').all()
+    return render(request, 'portfolio/ucs.html', {'ucs': ucs})
